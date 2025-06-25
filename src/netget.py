94b6889721a728ad7e8b967d45dc45ed7fd074d6
AnmiTaliDev <anmitali198@gmail.com>
2025-06-25 08:59:52 +0500

    Удалили мусор. Починил URL www.bbc.com.

--- src/netget.py
+++ src/netget.py
@@ -1,9 +1,12 @@
 import requests 
-from bs4 import BeautifulShop
+from bs4 import BeautifulSoup
 
-def parse_news(str: url):
+def parse_news(url: str):
     try:
-        response = requests.get(url)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, "html.parser")
 
@@ -25,8 +28,9 @@ def parse_news(str: url):
     except requests.RequestException as e:
         print(f"Error: {e}")
         return []
+
 def main():
-    url = "https://ww.bbc.com/news"
+    url = "https://www.bbc.com/news"
     news = parse_news(url)
     if news:
         print(f"Find: {len(news)}")
